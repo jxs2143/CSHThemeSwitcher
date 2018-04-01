2b4b0ed8539faf4c5eb9bfc2accc0f9dce0f38a8
Max Meinhold <mxmeinhold@gmail.com>
2018-03-31 21:22:03 -0400

    Implements api

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-/*var express = require('express');
-var app = express();
-
-var port = 8080;
-
-app.get('/user/:userID', function(req, res) {
-  res.send(req.params.userID);
-});
-
-
-app.listen(port);*/
-
 require('dotenv').config();
 
 var express = require('express');
 var passport = require('passport');
 var Strategy = require('passport-openidconnect').Strategy;
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI);
+
+var db = mongoose.connection;
+var User;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  var userSchema = mongoose.Schema({
+    uid: String,
+    css: String
+  });
+
+  User = mongoose.model('User', userSchema);
+});
 
 
 // Configure the OpenID Connect strategy for use by Passport.
@@ -68,8 +71,8 @@ var app = express();
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
+//app.use(require('morgan')('combined'));
+//app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
@@ -80,12 +83,12 @@ app.use(passport.session());
 
 
 // Define routes.
-app.get('/',
+/*app.get('/',
         function(req, res) {
   res.send("Root");
 
   //res.render('home', { user: req.user });
-});
+});*/
 
 app.get('/login',
         passport.authenticate('openidconnect'));
@@ -96,17 +99,45 @@ app.get('/login/callback',
   res.redirect('/');
 });
 
-app.get('/profile',
+/*app.get('/profile',
         require('connect-ensure-login').ensureLoggedIn(),
         function(req, res){
   res.send("Response");
   //res.render('profile', { user: req.user });
-});
+});*/
 
 app.get('/user/:userID',
         require('connect-ensure-login').ensureLoggedIn(),
-        function(req, res){
-
+        function(req, res) {
+  User.findOne({ 'uid' : req.params.userID }, function(err, user) {
+    if(user != null) res.send(user.css);
+    else {
+      res.send(process.env.DEFAULT_CSS);
+      if(err != null) console.error(err);
+    }
+  });
+});
+app.get('/user/:userID/:css',
+        require('connect-ensure-login').ensureLoggedIn(),
+        function(req, res) {
+  User.findOne({ 'uid' : req.params.userID}, function(err, user) {
+    if(err) console.error(err);
+    else {
+      console.log(req.params.userID + " : " + req.params.css);
+      if(user == null) {
+        var u = new User({ uid: req.params.userID, css: req.params.css });
+        u.save(function(err, u) {
+        });
+      }
+      else {
+        user.css = req.params.css;
+        user.save(function(err, user) {
+          if(err) console.error(err);
+        });
+      }
+    }
+  });
+  res.send('Submitted');
 });
 
 app.listen(3000);
